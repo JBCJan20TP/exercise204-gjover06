65d2e3bf30f31f044c36fb9edcbfba85c771e290
gjover06 <georgesamu@yahoo.com>
2020-01-09 11:10:59 -0500

    Completed assignment

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -1,12 +1,13 @@
 package com.example.demo;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.validation.Valid;
-
+@Controller
 public class HomeController {
     @GetMapping("/carform")
     public String loadCarForm(Model model){
@@ -14,7 +15,10 @@ public class HomeController {
         return "carform";
     }
     @PostMapping("/carform")
-    public String processCarForm(@Valid Car car, BindingResult){
-        if ()
+    public String processCarForm(@Valid Car car, BindingResult result){
+        if (result.hasErrors()){
+            return "carform";
+        }
+        return "carconfirm";
     }
 }
